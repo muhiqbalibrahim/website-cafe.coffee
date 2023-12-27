e9c79b995689806dde4a94226069c292f0411673
muhiqbalibrahim <muhiqbalibrahim962@gmail.com>
2023-12-27 10:22:15 +0800

    membuat website cafe coffee

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// TOGGLE CLASS ACTIVE
+const navbarNav = document.querySelector('.navbar-nav');
+
+// KETIKA HAMBURGER MENU DIKLIK
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// KETIKA MENGKLIK DI LUAR SIDEBAR UNTUK MENGHILANGKAN NAV
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+})
